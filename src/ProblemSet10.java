dfa9e9075fb5f378fe1a22d983e74d2a02255e7f
tikano <oadmathgenie@gmail.com>
2021-02-14 19:38:44 -0500

    TestHarness

--- src/ProblemSet10.java
+++ src/ProblemSet10.java
@@ -5,42 +5,43 @@ public class ProblemSet10 {
     }
 
     public String[] fizzBuzz(int start, int end) {
-
+        String[] result = {"a", "b"};
+        return result;
     }
 
     public int maxSpan(int[] numbers) {
-
+        return -1;
     }
 
     public int[] fix34(int[] numbers) {
-
+        return null;
     }
 
     public int[] fix45(int[] numbers) {
-
+        return null;
     }
 
     public boolean canBalance(int[] numbers) {
-
+        return false;
     }
 
     public boolean linearIn(int[] outer, int[] inner) {
-
+        return false;
     }
 
     public int[] squareUp(int n) {
-
+        return null;
     }
 
     public int[] seriesUp(int n) {
-
+        return null;
     }
 
     public int maxMirror(int[] numbers) {
-
+        return -1;
     }
 
     public int countClumps(int[] numbers) {
-
+        return -1;
     }
 }
---
+++ src/TestHarness.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class TestHarness {
+    public static void main(String[] args) {
+        ProblemSet10 ps = new ProblemSet10();
+    
+        int[] iArr = {};
+        String[] sArr = {};
+
+        boolean b = false;
+        int i = 0;
+
+        int[] input1 = {1, 2, 1, 4, 5, 2};
+        int[] input2 = {1, 2, 3};
+
+        int int1 = -9;
+        int int2 = 4;
+
+        //Exercise 1
+        //sArr = ps.fizzBuzz(int1, int2);
+        //System.out.printf("%s\n", Arrays.toString(sArr)); 
+
+        //Exercise 2
+        //i = ps.maxSpan(input1);
+        //System.out.printf("%d\n", i);        
+
+        //Exercise 3
+        //iArr = ps.fix34(input1);
+        //System.out.printf("%s\n", Arrays.toString(iArr));        
+
+        //Exercise 4
+        //iArr = ps.fix45(input1);
+        //System.out.printf("%s\n", Arrays.toString(iArr));        
+
+        //Exercise 5
+        //b = ps.canBalance(input1);
+        //System.out.printf("%s\n", b);   
+
+        //Exercise 6
+        //b = ps.linearIn(input1, input2);
+        //System.out.printf("%s\n", b);      
+
+        //Exercise 7
+        //iArr = ps.squareUp(int1);
+        //System.out.printf("%s\n", Arrays.toString(iArr));        
+
+        //Exercise 8
+        //iArr = ps.seriesUp(int1);
+        //System.out.printf("%s\n", Arrays.toString(iArr));        
+
+        //Exercise 9
+        //i = ps.maxMirror(input1);
+        //System.out.printf("%d\n", i);        
+
+        //Exercise 10
+        //i = ps.countClumps(input1);
+        //System.out.printf("%d\n", i);        
+        
+    }
+
+}
